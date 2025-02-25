b886d4f00d82d0bd63f538ff5b6047a390ede246
darcyclarke <darcy@darcyclarke.me>
2025-02-25 11:26:13 -0500

    chore: move/remove legacy tooling

--- test/spec.js
+++
@@ -1,8 +0,0 @@
-import { manifest } from '@vltpkg/package-info'
-import { Spec } from '@vltpkg/spec'
-
-const lodash = Spec.parse('lodash@latest')
-console.log(lodash)
-
-const mani = await manifest('lodash@latest')
-console.log(mani)
